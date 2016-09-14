7c85978dcd742b72834bbd6ed2621c0d40e8ee31
mitchellnau <mmh327@nau.edu>
2016-09-13 20:17:59 -0700

    file opening/closing and error checking

--- ppmrw.c
+++ ppmrw.c
@@ -15,14 +15,30 @@ int main(int argc, char* argv[]){
   printf("Args: %s\n", argv[0]);
   printf("Args: %s\n", argv[1]);
   printf("Args: %s\n", argv[2]);
-  printf("Args: %s\n", argv[3]);
+  printf("Args: %s\n\n\n", argv[3]);
 
   if(argc < 4)
-    fprintf(stderr, "Insufficient parameter amount.\nProper input: input_filename output_filename\n\n", 001);
+    fprintf(stderr, "Error: Insufficient parameter amount.\nProper input: input_filename output_filename\n\n", 001);
+
   if(argc > 4)
-    fprintf(stderr, "Too many parameters.\nProper input: input_filename output_filename\n\n", 002);
+    fprintf(stderr, "Error: Too many parameters.\nProper input: input_filename output_filename\n\n", 002);
+
   if(argc == 4){
-    printf("Good job.");
+    FILE * inputfp;
+    FILE * outputfp;
+    inputfp = fopen(argv[2], "r");
+    outputfp = fopen(argv[3], "w");
+    if (inputfp == 0)
+      fprintf(stderr, "Error: Input file \"%s\" could not be opened.\n", argv[2], 003);
+    if (outputfp == 0)
+      fprintf(stderr, "Error: Output file \"%s\" could not be opened.\n", 004, argv[3]);
+
+
+
+
+    fclose(inputfp);
+    fclose(outputfp);
   }
+  printf("closing...");
   return(0);
 };
