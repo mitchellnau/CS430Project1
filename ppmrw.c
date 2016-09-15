87fa014b70100dcff9bcdc92e4bcb2faccff2f5b
mitchellnau <mmh327@nau.edu>
2016-09-15 08:42:52 -0700

    input checking & decision, buffer made global

--- ppmrw.c
+++ ppmrw.c
@@ -7,13 +7,13 @@
 
 FILE* inputfp;
 FILE* outputfp;
+char data[20000];
 
-char* read_p3(){
-  printf("Args: %d\n", inputfp);
-  char* str;
-  char* data;
+int read_p3(){
+  //printf("Fd: %d\n", inputfp);
   int i, line, endOfHeader, x;
   char a, b;
+  char* str;
 
 
   a = fgetc(inputfp);
@@ -21,7 +21,7 @@ char* read_p3(){
   data[0] = a;
   data[1] = b;
   if(a != 'P' || b !='3'){
-    fprintf(stderr, "Error: Improper header filetype.\n", 005);
+    fprintf(stderr, "Error: Improper header filetype.\n", 006);
   }
   a = fgetc(inputfp);
   data[2] = a;
@@ -38,31 +38,36 @@ char* read_p3(){
       line++;
   }
   data[i] = '\n';
+  i++;
+  data[i] = '\n';
 
   endOfHeader = i+1;
 
 
-  printf("\nBuffer:\n%s-------------\n", data);
+  //printf("\nBuffer:\n%s-------------\n", data);
 
 
   for(i=0; !feof(inputfp) ; i++){
     a = fgetc(inputfp);
     //printf("%c", a);
     if(a != EOF)
-      data[i+endOfHeader] = a;
+      data[i+endOfHeader-1] = a;
   }
 
   data[i+endOfHeader-1] = '\0';
-  printf("\nBuffer:\n%s-------------\n", data);
+  //printf("\nBuffer:\n%s-------------\n", data);
 
-  return data;
+  return 1;
 }
 
 
-void write_p3();
+void write_p3(){
+};
 
-void read_p6();
-void write_p6();
+void read_p6(){
+};
+void write_p6(){
+};
 
 
 
@@ -72,7 +77,7 @@ int main(int argc, char* argv[]){
   printf("Args: %s\n", argv[0]);
   printf("Args: %s\n", argv[1]);
   printf("Args: %s\n", argv[2]);
-  printf("Args: %s\n\n\n", argv[3]);
+  printf("Args: %s\n", argv[3]);
 
   if(argc < 4)
     fprintf(stderr, "Error: Insufficient parameter amount.\nProper input: input_filename output_filename\n\n", 001);
@@ -82,22 +87,33 @@ int main(int argc, char* argv[]){
 
   if(argc == 4){
     inputfp = fopen(argv[2], "r");
+    outputfp = fopen(argv[3], "w");
     if (inputfp == 0)
       fprintf(stderr, "Error: Input file \"%s\" could not be opened.\n", argv[2], 003);
-
-
-    char* dataToWrite;
-    dataToWrite = read_p3();
-
-    outputfp = fopen(argv[3], "w");
-
     if (outputfp == 0)
-      fprintf(stderr, "Error: Output file \"%s\" could not be opened.\n", 004, argv[3]);
+      fprintf(stderr, "Error: Output file \"%s\" could not be opened.\n", argv[3], 004);
 
-    fprintf(outputfp, "%s", dataToWrite);
+    else{
+      if (argv[1][0] == '3' || argv[1][0] == '6'){
+            if(argv[1][0] == '3'){
+                read_p3();
+                write_p6();
+            }
+            else{
+                read_p6();
+                write_p3();
+            }
+      }
+      else {
+        fprintf(stderr, "Error: Input number \"%s\" is not 3 or 6.\n", argv[1], 005);
+      }
+    //printf("\nBuffer:\n%s---okay---\n", read_p3());
+
+    //fprintf(outputfp, "%s", read_p3());
 
     fclose(outputfp);
     fclose(inputfp);
+    }
   }
   printf("closing...");
   return(0);
